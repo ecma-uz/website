bf22a530f3c0fd07f355e4061298566d528b8405
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-16 00:16:31 +0500

    chore: deploy website to gh pages

---
+++ default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+    import nixpkgs {overlays = [];},
+  ...
+}: let
+  manifest = pkgs.lib.importJSON ./package.json;
+in
+  # pkgs.stdenv.mkDerivation {
+  pkgs.buildNpmPackage {
+    pname = manifest.name;
+    version = manifest.version;
+
+    src = ./.;
+    npmDepsHash = "sha256-y4fY8c1UeRMiHpjjAbqYD1eaAIf5I075jDk7peA7H54=";
+
+    installPhase = ''
+      # Create output directory
+      mkdir -p $out
+
+      # Copy standalone as library
+      cp -R ./out/* $out/
+    '';
+
+    nativeBuildInputs = with pkgs; [
+      # Typescript
+      nodejs
+      pnpm
+      corepack
+
+      # Hail the Nix
+      nixd
+      statix
+      alejandra
+    ];
+
+    buildInputs = with pkgs; [
+      vips
+      openssl
+    ];
+
+    NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+    meta = with pkgs.lib; {
+      homepage = "https://ecma.uz";
+      description = manifest.description;
+      license = with licenses; [cc-by-40];
+      platforms = with platforms; linux ++ darwin;
+      maintainers = with maintainers; [orzklv];
+    };
+  }
---
+++ shell.nix
@@ -0,0 +1,41 @@
+{
+  pkgs ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+    import nixpkgs {overlays = [];},
+  ...
+}: let
+  manifest = pkgs.lib.importJSON ./package.json;
+in
+  pkgs.stdenv.mkDerivation {
+    name = manifest.name;
+
+    nativeBuildInputs = with pkgs; [
+      # Typescript
+      deno
+      nodejs
+      pnpm
+      corepack
+      eslint
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+
+      # Hail the Nix
+      nixd
+      statix
+      deadnix
+      alejandra
+
+      # Tailwind
+      tailwindcss
+    ];
+
+    buildInputs = with pkgs; [
+      openssl
+      vips
+    ];
+  }
